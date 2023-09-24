cbcfbc5903dea729ec4c1fb054b3395c8fee7961
fabianatellechea <fabianagtellechea@gmail.com>
2023-09-24 15:56:08 -0700

    fixed: logic error task 2

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list:
+        maxx = my_list[0]
         for i in my_list:
-            maxx = i
-            for j in my_list:
-                if i < j:
-                    maxx = j
+            if i > maxx:
+                maxx = i
 
-                    return maxx
+        return maxx
     return None
