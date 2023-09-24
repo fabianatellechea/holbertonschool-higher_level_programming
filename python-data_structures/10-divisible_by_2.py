0385f42916734b9182d90f868c199b552d746183
fabianatellechea <fabianagtellechea@gmail.com>
2023-09-24 16:16:01 -0700

    fixed: task 10 returns

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     new_list = []
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
-            return new_list.append(True)
+            new_list.append(True)
         else:
-            return new_list.append(False)
+            new_list.append(False)
     return new_list
