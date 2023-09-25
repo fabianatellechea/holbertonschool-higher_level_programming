a7863ba8cab90b27d500d181a2ff69c16d83dcf9
fabianatellechea <fabianagtellechea@gmail.com>
2023-09-24 17:36:34 -0700

    try: fixing requirements

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-import sys
+from sys import argv
 
 if __name__ == "__main__":
 
-    argc = len(sys.argv) - 1
-    op = sys.argv[2]
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
+    argc = len(argv) - 1
+    op = argv[2]
+    a = int(argv[1])
+    b = int(argv[3])
 
     if argc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
