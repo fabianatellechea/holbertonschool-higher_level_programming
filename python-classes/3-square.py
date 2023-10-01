073fdc3297cc01f395f956bb18f1b03fa5f3c5f3
fabianatellechea <fabianagtellechea@gmail.com>
2023-10-01 11:41:24 -0700

    added:task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" creating class """
+
+
+class Square:
+    """ class """
+
+    def __init__(self, size=0):
+        self.__size = size
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size * self.__size
