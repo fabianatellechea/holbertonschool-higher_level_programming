be9bef5cb705fa6f9e8cba7f7fe86c84f161ab0e
fabianatellechea <fabianagtellechea@gmail.com>
2023-10-03 21:17:45 -0700

    added: task 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -8,6 +8,16 @@ class Rectangle:
         self.__height = height
         self.__width = width
 
+        if type(self.__width) is not int:
+            raise TypeError("width must be an integer")
+        if self.__width < 0:
+            raise ValueError("width must be >= 0")
+
+        if type(self.__height) is not int:
+            raise TypeError("height must be an integer")
+        if self.__height < 0:
+            raise ValueError("height must be >= 0")
+
     @property
     def width(self):
         return self.__width
