c4a20fe91bb5d78b0baa26267125b79b06684d81
fabianatellechea <fabianagtellechea@gmail.com>
2023-10-07 15:45:44 -0700

    added: task 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""creating class """
+
+
+class Student:
+    """class """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        res = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                res[attr] = getattr(self, attr)
+        return res
+
+    def reload_from_json(self, json):
+        return self.__dict__.update(json)
