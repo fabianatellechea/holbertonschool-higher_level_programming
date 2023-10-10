b17e89d259ba152ffb424cdd05f24bcc23dbac4b
root <fabianagtellechea@gmail.com>
2023-10-09 17:09:45 -0700

    added: task 5

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -83,3 +83,9 @@ class Rectangle(Base):
     def area(self):
         """ return area"""
         return self.__width * self.__height
+
+    def display(self):
+        """prints in stdout the Rectangle instance
+        with the character #"""
+        for i in range(self.__height):
+            print('#' * self.__width)
